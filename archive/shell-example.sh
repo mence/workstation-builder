449951c7f596785e18a2eaaa3dc95199cab7f4fd
Tim Hordern <tim.hordern@gmail.com>
2017-08-16 16:48:40 +1000

    Archive old files prior to cleanup.

---
+++ archive/shell-example.sh
@@ -0,0 +1,102 @@
+#!/bin/sh
+
+###############################################################################
+# Shell
+# https://gist.github.com/saetia/1623487
+###############################################################################
+
+# Homebrew
+# install package manager
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew analytics off
+
+# BREWFILE
+
+brew update
+brew doctor
+brew upgrade
+brew doctor
+brew cleanup
+
+# Turn off extra analytics
+brew analytics off
+
+# Install Bundle
+brew tap homebrew/bundle
+brew bundle
+
+# System maintenance
+brew update
+brew doctor
+brew upgrade
+brew doctor
+brew cleanup
+
+# install homebrew packages
+brew install \
+tree \
+node \
+ssh-copy-id \
+wget \
+jpegoptim \
+pngcrush \
+colordiff \
+ghostscript \
+imagemagick --with-ghostscript \
+graphicsmagick \
+ack
+
+#Homebrew Web Server Packages
+brew install \
+dnsmasq \
+nginx \
+mariadb \
+redis \
+memcached \
+libmemcached \
+
+# Homebrew Cask Apps & Fonts
+# add support for fonts
+brew tap caskroom/fonts
+
+#add dev/beta versions
+brew tap caskroom/versions
+
+#install mac apps & fonts
+brew cask install \
+font-source-code-pro \
+adobe-creative-cloud \
+blueharvest \
+cleanmymac \
+cocktail \
+ghostlab \
+coda \
+sublime-text-dev \
+virtualbox \
+coderunner \
+google-chrome \
+firefox \
+codekit \
+iterm2-beta \
+sequel-pro \
+querious \
+imageoptim \
+imagealpha \
+xquartz \
+simpholders-2-alpha \
+handbrake \
+vagrant \
+ksdiff \
+spotify
+
+# Switch to z-shell
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+# Update .zshrc
+wget https://gist.githubusercontent.com/saetia/2764210/raw/ab099b587689640eb32cbc1afdb6a19b62be7fb0/.zshrc -O \
+~/.zshrc
+
+#syntax highlighting
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git \
+~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
