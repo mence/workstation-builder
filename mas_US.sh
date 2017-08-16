24bffde23f500a413dfe0a65218c39880d8419d8
Tim Hordern <tim.hordern@gmail.com>
2017-08-16 15:14:19 +1000

    Add Mac App Store installs using mas

---
+++ mas_US.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "Installing Mac App Store Applications in US account..."
+
+# Twitter
+mas install 409789998
+# TweetDeck by Twitter
+mas install 485812721
+
+# GrowlTunes
+mas install 499190094
+
+# WiFi Explorer
+mas install 494803304
+
+# DaisyDisk
+mas install 411643860
+
+# Reeder 3
+# mas install 880001334 # not actually purchased!
+
+# Day One 
+# mas install 1055511498 # not actually purchased!
+
+# Alternote — the Beautiful Note-taking App for Evernote
+mas install 974971992
+
+# Be Focused Pro - Focus Timer & Goal Tracker
+mas install 961632517
+
+# Spillio
+mas install 873245660
+
+# Marked 2
+mas install 890031187
+
+# Noizio
+mas install 928871589
