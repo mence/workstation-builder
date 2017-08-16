29ab4319f59e84433bb8b8da3e5586978accc736
Tim Hordern <tim.hordern@gmail.com>
2017-08-16 15:09:58 +1000

    Cleanup rvm installation. Add additional ruby versions

--- ruby.sh
+++ ruby.sh
@@ -2,15 +2,23 @@
 
 # TODO: Port to Brewfile 'brew gem install foo': https://github.com/sportngin/brew-gem
 
-# RVM
-curl -sSL https://get.rvm.io | bash -s stable
+echo "Installing Ruby apps..."
 
-# Install all the versions
-rvm install 1.8.7-head
+# RVM
+echo "Install rvm"
+#gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+command curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+\curl -sSL https://get.rvm.io | bash -s stable
+source /Users/timhordern/.rvm/scripts/rvm
+
+echo "Install all the ruby versions we need"
+#rvm install 1.8.7-head # broken
 rvm install 1.9.3
 rvm install 2.0.0
 rvm install 2.2
+rvm install 2.2.1
 rvm install 2.3
+rvm install 2.4
 
 rvm use system --default
 
