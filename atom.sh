27fcb29415d81a14e1fcfdf4761fa00e2655e952
Tim Hordern <tim.hordern@gmail.com>
2017-08-16 15:08:23 +1000

    Add Atom package installation

---
+++ atom.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+echo "Installing Atom plugins..."
+
+apm install activate-power-mode
+apm install atom-beautify
+apm install atom-ctags
+apm install atomify
+apm install autoclose-html
+apm install autocomplete-ctags
+apm install autocomplete-modules
+apm install colorblind-wombat-dark-syntax
+apm install ctags-status
+apm install cucumber
+apm install cucumber-autocomplete
+apm install cucumber-step
+apm install docblockr
+apm install docker
+apm install editorconfig
+apm install file-icons
+apm install git-time-machine
+apm install github-atom-light-syntax
+apm install highlight-selected
+apm install javascript-snippets
+apm install language-babel
+apm install language-docker
+apm install language-dotenv
+apm install language-ini
+apm install linter
+apm install linter-eslint
+apm install linter-flow
+apm install open-recent
+apm install pigments
+apm install react
+apm install sort-lines
+apm install tasks
+apm install todo-show
+apm install toggle-quotes
+apm install wakatime
+
+# Suggested installs
+
+# apm install seti-ui
+# apm install monokai-seti
+# apm install linter-htmlhint
+# apm install linter-csslint
+# apm install auto-detect-indentation
+# apm install rest-client
+# apm install regex-railroad-diagram
+# apm install rulerz
