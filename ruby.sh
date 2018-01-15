e5fea15290a30c220c0ef69e7f7a234411554dce
Tim Hordern <tim.hordern@gmail.com>
2018-01-16 10:37:28 +1100

    Update rvm configuration steps and ruby version installs

--- ruby.sh
+++ ruby.sh
@@ -17,8 +17,20 @@ rvm install 1.9.3
 rvm install 2.0.0
 rvm install 2.2
 rvm install 2.2.1
-rvm install 2.3
-rvm install 2.4
+rvm install 2.2.7
+rvm install 2.3.0
+rvm install 2.3.1
+rvm install 2.4.0
+rvm install 2.4.1
+rvm install 2.4.2
+
+rvm docs generate-ri
+rvm rvmrc warning ignore allGemfiles
+
+rvm get stable
+rvm reload
+rvm osx-ssl-certs update all
+rvm rubygems latest
 
 rvm use system --default
 
