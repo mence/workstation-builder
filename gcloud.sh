ed489ee92a018d0c03cbae204b9cdbc7452bbec6
Tim Hordern <tim.hordern@gmail.com>
2018-08-31 12:45:32 +1000

    Add additional commands to Google Cloud installation

--- gcloud.sh
+++ gcloud.sh
@@ -7,8 +7,14 @@ gcloud components update
 gcloud components install beta
 gcloud components install kubectl
 gcloud components install alpha
+gcloud components install docker-credential-gcr
 
 gcloud auth login
 gcloud auth application-default login
+gcloud auth configure-docker
 
 gcloud compute project-info add-metadata --metadata google-compute-default-region=australia-southeast1,google-compute-default-zone=australia-southeast1-c
+
+# https://cloud.google.com/container-registry/docs/advanced-authentication
+docker-credential-gcr configure-docker
+docker-credential-gcr gcr-login
