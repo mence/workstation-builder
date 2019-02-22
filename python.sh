fc83ae14559257d823a65cfd4885dad435d8c7e6
Tim Hordern <tim.hordern@gmail.com>
2019-02-22 14:36:56 +1100

    Remove pyenv addon installs as now in Brewfile, add new version of Python to pyenv install

--- python.sh
+++ python.sh
@@ -13,26 +13,13 @@ if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -
 
 # pyenv installs: https://github.com/yyuu/pyenv
 echo "Install all the Python versions we need"
+pyenv install 3.7.2 --verbose
 pyenv install 3.5.0 --verbose
 pyenv install 3.4.3 --verbose
 pyenv install 3.3.6 --verbose
 pyenv install 3.2.6 --verbose
 pyenv install 2.7.10 --verbose
 pyenv install 2.6.9 --verbose
-
-# pyenv plugins
-# pyenv-virtualenv: https://github.com/yyuu/pyenv-virtualenv
-# echo "Install pyenv-virtualenv"
-# brew install pyenv-virtualenv # Moved to Brewfile
-# echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile # TODO: Move to .dotfiles
-
-# pyenv-virtualenvwrapper: https://github.com/yyuu/pyenv-virtualenvwrapper
-# echo "Install pyenv-virtualenvwrapper"
-# brew install pyenv-virtualenvwrapper
-# TODO: Add to dotfiles: export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
-
-# Add Autoenv to bashrc
-# echo 'source /usr/local/opt/autoenv/activate.sh' >> ~/.bashrc
 
 # --- GENERAL ---
 
