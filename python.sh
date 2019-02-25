76ef550920c8d6c2820ce3e74402ec9b9576273d
Tim Hordern <tim.hordern@gmail.com>
2019-02-25 12:07:35 +1100

    Add explicit dependencies for python installs

--- python.sh
+++ python.sh
@@ -27,18 +27,16 @@ pip install waybackpack
 #pip install hogwatch
 #pip install voltron
 
-# tq: https://github.com/plainas/tq
-sudo pip3 install --upgrade html5lib==1.0b8
-sudo pip3 install https://github.com/plainas/tq/zipball/stable
-
 # Development
 pip install cookiecutter # https://github.com/audreyr/cookiecutter
-pip install requests: # http://docs.python-requests.org/en/latest/
+pip install requests # http://docs.python-requests.org/en/latest/
 pip install flask # http://flask.pocoo.org/
 pip install gixy # https://github.com/yandex/gixy
 
 # Testing
-pip3 install trypackage # https://github.com/timofurrer/try
+pip3 install trypackage # https://github.com/timofurrer/try\
+pip install --ignore-dependencies requests==2.20.1
+pip install selenium==3.141.0
 pip install bzt # https://github.com/Blazemeter/taurus
 pip install hamms # https://github.com/kevinburke/hamms
 
@@ -46,7 +44,9 @@ pip install hamms # https://github.com/kevinburke/hamms
 pip install voltron # https://github.com/snare/voltron
 
 # UI Applications
-sudo pip install youtube-dlg # https://github.com/MrS0m30n3/youtube-dl-gui
+pip install -U wxPython
+pip install -U twodict
+pip install youtube-dlg # https://github.com/MrS0m30n3/youtube-dl-gui
 
 # Scrapers
 pip3 install you-get # https://github.com/soimort/you-get
@@ -63,6 +63,7 @@ pip3 install pinggraph # https://github.com/orf/gping
 pip install pygments # http://pygments.org/
 
 # Random
+pip install pyyaml==3.11
 pip install twitter_markov # https://github.com/fitnr/twitter_markov
 pip install rtv # Command-line Reddit client
 
@@ -70,6 +71,7 @@ pip install rtv # Command-line Reddit client
 pip install aws-shell # https://github.com/awslabs/aws-shell
 
 # Docker
+pip install --ignore-installed requests==2.5.3
 pip install docker-utils # https://github.com/andymckay/docker-utils
 
 # Deployment
@@ -82,3 +84,8 @@ pip install pycodestyle # https://github.com/SublimeLinter/SublimeLinter-pycodes
 
 # Downloads
 pip3 install xkcd-dl # https://github.com/tasdikrahman/xkcd-dl
+
+# tq: https://github.com/plainas/tq
+sudo pip3 install --upgrade html5lib==1.0b8
+pip3 install beautifulsoup4==4.4.0
+sudo pip3 install https://github.com/plainas/tq/zipball/stable
