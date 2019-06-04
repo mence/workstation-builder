bb53e79ff34e588e353944b13c86e79364bf0f14
Tim Hordern <tim.hordern@gmail.com>
2019-06-04 18:50:43 +1000

    Be way more expressive in rvm install, and fix openssl reference bug

--- ruby.sh
+++ ruby.sh
@@ -10,21 +10,73 @@ gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D69
 \curl -sSL https://get.rvm.io | bash -s stable
 source /Users/timhordern/.rvm/scripts/rvm
 
+rvm get master # update to latest RVM just in case
+
 echo "Install all the ruby versions we need"
 #rvm install 1.8.7-head # broken
-rvm install 1.9.3
-rvm install 2.0.0
-rvm install 2.2
-rvm install 2.2.1
-rvm install 2.2.2
-rvm install 2.2.7
-rvm install 2.3.0
-rvm install 2.3.1
-rvm install 2.4.0
-rvm install 2.4.1
-rvm install 2.4.2
-rvm install 2.5.1
-rvm install 2.6.1
+rvm install 1.9.3 --with-openssl-dir=`brew --prefix openssl`
+rvm use 1.9.3
+gem install bundler
+
+rvm install 2.0.0 --with-openssl-dir=`brew --prefix openssl`
+rvm use 2.0.0
+gem install bundler
+
+rvm install 2.2 --with-openssl-dir=`brew --prefix openssl`
+rvm use 2.2
+gem install bundler
+
+rvm install 2.2.1 --with-openssl-dir=`brew --prefix openssl`
+rvm use 2.2.1
+gem install bundler
+
+rvm install 2.2.2 --with-openssl-dir=`brew --prefix openssl`
+rvm use 2.2.2
+gem install bundler
+
+rvm install 2.2.7 --with-openssl-dir=`brew --prefix openssl`
+rvm use 2.2.7
+gem install bundler
+
+rvm install 2.3.0 --with-openssl-dir=`brew --prefix openssl`
+rvm use 2.3.0
+gem install bundler
+
+rvm install 2.3.1 --with-openssl-dir=`brew --prefix openssl`
+rvm use 2.3.1
+gem install bundler
+
+rvm install 2.4.0 --with-openssl-dir=`brew --prefix openssl`
+rvm use 2.4.0
+gem install bundler
+
+rvm install 2.4.1 --with-openssl-dir=`brew --prefix openssl`
+rvm use 2.4.1
+gem install bundler
+
+rvm install 2.4.2 --with-openssl-dir=`brew --prefix openssl`
+rvm use 2.4.2
+gem install bundler
+
+rvm install 2.5.1 --with-openssl-dir=`brew --prefix openssl`
+rvm use 2.5.1
+gem install bundler
+
+rvm install 2.6.1 --with-openssl-dir=`brew --prefix openssl`
+rvm use 2.6.1
+gem install bundler
+
+rvm install 2.6.1 --with-openssl-dir=`brew --prefix openssl`
+rvm use 2.6.3
+gem install bundler
+
+rvm install 2.7 --with-openssl-dir=`brew --prefix openssl`
+rvm use 2.7
+gem install bundler
+
+rvm install ruby-head --with-openssl-dir=`brew --prefix openssl`
+rvm use ruby-head
+gem install bundler
 
 rvm docs generate-ri
 rvm rvmrc warning ignore allGemfiles
