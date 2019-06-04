8e28dab505b22a9d17c5c31ac3d803e261b79731
Tim Hordern <tim.hordern@gmail.com>
2019-06-04 15:46:39 +1000

    Update ruby installation script for new rvm keys and install. Add ruby 2.6.1 to installed rubies.

--- ruby.sh
+++ ruby.sh
@@ -6,8 +6,7 @@ echo "Installing Ruby apps..."
 
 # RVM
 echo "Install rvm"
-#gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-command curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
 source /Users/timhordern/.rvm/scripts/rvm
 
@@ -25,6 +24,7 @@ rvm install 2.4.0
 rvm install 2.4.1
 rvm install 2.4.2
 rvm install 2.5.1
+rvm install 2.6.1
 
 rvm docs generate-ri
 rvm rvmrc warning ignore allGemfiles
